91965d775402a5994daf4a4a27f89e67ccc7968f
pernillehm <56149661+pernillehm@users.noreply.github.com>
2020-09-30 12:58:00 +0200

    alt muligt

---
+++ js/main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+function showLoader(show) {
+    let loader = document.querySelector('#loader');
+    if (show) {
+        loader.classList.remove("hide");
+    } else {
+        loader.classList.add("hide");
+    }
+}
+
+showLoader(true);
